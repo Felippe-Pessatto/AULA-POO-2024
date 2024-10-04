193315619526e80bf5bf626516b5ac00bcc16140
Felippe-Pessatto <105829944+Felippe-Pessatto@users.noreply.github.com>
2024-10-04 18:40:38 -0300

    Update and rename EXERCICIO 01.txt to EXERCICIO 01.py

---
+++ EXERCICIO 01.py
@@ -0,0 +1,10 @@
+EM CÓDIGO PYTHON RESPONDA:
+
+01 - O QUE É PYTHON?
+>>> print("Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.")
+
+02 - Qual programa (IDE) executa o arquivo .py?
+>>> print("O programa IDE Visual Studio Code e o programa IDE Pycharm executam o arquivo .py")
+
+03 - Qual Linguagem de programação Python substituiu?
+>>> print("Python substituiu a linguagem de programação ABC")
