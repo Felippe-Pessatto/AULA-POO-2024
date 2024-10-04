73e7407a24f941268ccf9b152d30acb9daf688c6
Felippe-Pessatto <105829944+Felippe-Pessatto@users.noreply.github.com>
2024-10-04 20:03:27 -0300

    Add files via upload

---
+++ EXERCICIO 04 - Q03.py
@@ -0,0 +1,10 @@
+"03 - COM UMA PERSONA INSIRA OS SEGUINTES DADOS DE UM FORMULÁRIO: NOME, SOBRENOME, IDADE, CIDADE, ESTADO, E-MAIL E TELEFONE PARA CONTATO. EM SEGUIDA APRESENTE A SEGUINTE MENSAGEM: USUÁRIO XXX XXXX NASCIMENTO NO ANO DE; XXXX DA CIDADE XXX, ESTADO XX, PORTADOR DO TELEFONE; XXX, COM SEU PRINCIPAL E-MAIL; XXX, FOI CADASTRADO COM SUCESSO."
+nome = input("Insira seu nome: ")
+sobrenome = input("Insira seu sobrenome: ")
+idade = int(input("Insira sua idade: "))
+cidade = input("Insira sua cidade: ")
+estado = input("Insira seu estado: ")
+email = input("Insira seu e-mail: ")
+telefone = input("Insira seu telefone: ")
+
+print("Usuário",nome, sobrenome,"nascimento no ano de;",(2024-idade),"da cidade",cidade,"estado",estado,"portador do telefone;",telefone,"com seu principal e-mail;",email,"foi cadastrado com sucesso.")
---
+++ EXERCIOCIO 04 - Q01.py
@@ -0,0 +1,10 @@
+print("Aula poo dia 04/10/2024")
+"01 - MÉDIA DE QUATRO NÚMEROS: INFORME QUATRO NÚMEROS E MOSTRA A MÉDIA DELES."
+n1 = int(input("Informe a 1 nota: "))
+n2 = int(input("Informe a 2 nota: "))
+n3 = int(input("Informe a 3 nota: "))
+n4 = int(input("Informe a 4 nota: "))
+
+calc = (n1+n2+n3+n4)/4
+
+print("Media é: ",calc)
