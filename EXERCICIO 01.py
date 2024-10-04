e52db94c84eef4e08f996626695cb689950f8373
Felippe-Pessatto <105829944+Felippe-Pessatto@users.noreply.github.com>
2024-10-04 19:32:00 -0300

    Update EXERCICIO 01.py

--- EXERCICIO 01.py
+++ EXERCICIO 01.py
@@ -1,10 +1,10 @@
-EM CÓDIGO PYTHON RESPONDA:
+"EM CÓDIGO PYTHON RESPONDA:"
 
-01 - O QUE É PYTHON?
+"01 - O QUE É PYTHON?"
 >>> print("Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.")
 
-02 - QUAL PROGRAMA (IDE) EXECUTA O ARQUIVO .py?
+"02 - QUAL PROGRAMA (IDE) EXECUTA O ARQUIVO .py?"
 >>> print("O programa IDE Visual Studio Code e o programa IDE Pycharm executam o arquivo .py")
 
-03 - QUAL LINGUAGEM DE PROGRAMAÇÃO PYTHON SUBSTITUIU?
+"03 - QUAL LINGUAGEM DE PROGRAMAÇÃO PYTHON SUBSTITUIU?"
 >>> print("Python substituiu a linguagem de programação ABC")
