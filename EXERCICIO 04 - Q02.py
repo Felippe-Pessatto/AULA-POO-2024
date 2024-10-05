3f96d22125daa03cab63867aed76e1abd5cfa5e1
Felippe-Pessatto <105829944+Felippe-Pessatto@users.noreply.github.com>
2024-10-04 21:41:40 -0300

    Delete EXERCICIO 04 - Q02.py

--- EXERCICIO 04 - Q02.py
+++
@@ -1,6 +0,0 @@
-"02 - USUÁRIO INFORMA A QUANTIDADE EM HORAS, EM SEGUIDA CONVERTA PARA MINUTOS E SEGUNDOS. APRESENTE O RESULTADO."
-horas = int(input("Informe as horas: "))
-minutos = (horas)*60
-segundos = (minutos)*60
-
-print("A hora informada tem",minutos,"minutos e",segundos,"segundos")
