b12b55331eca8d012bbefe11b3d1a78029374aa8
Felippe-Pessatto <105829944+Felippe-Pessatto@users.noreply.github.com>
2024-10-05 07:41:24 -0300

    Update LISTA 01 - Q09.py

--- LISTA 01 - Q09.py
+++ LISTA 01 - Q09.py
@@ -1,4 +1,5 @@
 "CONVERSÃO DE MOEDA: PEÇA UM VALOR EM REAIS E MOSTRE O VALOR CONVERTIDO EM DÓLARES. CONSIDERE UMA TAXA DE CONVERSÃO FIXA."
 
-numero1 = int(input("Informe a temperatura Celsius: "))
-print("A temperatura em Fahrenheit é",(numero1*9/5)+32)
+valor_Real = float(input("Informe quantidade em reais: "))
+taxa_Dolar = 5.46
+print("O valor em doláres é: ",(valor_Real*taxa_Dolar))
