8d8cf7831d72b30c608b04507012599ba352d6f6
Felippe-Pessatto <105829944+Felippe-Pessatto@users.noreply.github.com>
2024-10-05 15:10:30 -0300

    Update LISTA 01 - Q05.py

--- LISTA 01 - Q05.py
+++ LISTA 01 - Q05.py
@@ -4,3 +4,5 @@ numero1 = int(input("Informe um número: "))
 numero2 = int(input("Informe um número: "))
 quociente = int(numero1/numero2)
 print("O resto da divisão do primeiro número pelo segundo número é:",numero1-(numero2*quociente))
+"ou"
+print("O resto da divisão do primeiro número pelo segundo número é:",numero1%numero2)
